2503084b44a91ae9003c640795d997fde9b7aa2c
Jakub Wronowski <jakubwro@gmail.com>
2023-09-25 15:20:54 +0200

    fix fallback handler

--- test/fallback_handler.jl
+++ test/fallback_handler.jl
@@ -23,11 +23,11 @@ end
             @info "Custom fallback called." msg
         end
     )
-    sub = subscribe("SOME.BAR") do msg
+    sub = subscribe("SOME.FOO") do msg
         @show msg
     end
     empty!(NATS.state.handlers) # Break state of connection to force fallback handler.
-    publish("SOME.BAR"; payload = "Hi!")
+    publish("SOME.FOO"; payload = "Hi!")
     sleep(0.5) # Wait for compilation.
     @test nc.stats.msgs_not_handled > 0
     @test was_called
