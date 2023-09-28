43687a4a93014c9b2e61632bef40e1c9f947e098
Jakub Wronowski <jakubwro@gmail.com>
2023-09-28 21:18:50 +0200

    wait for reconnect before send

--- src/connect.jl
+++ src/connect.jl
@@ -100,6 +100,10 @@ end
 function sendloop(nc::Connection, io::IO)
     mime = MIME_PROTOCOL() 
     while true
+        if !isopen(nc.outbox)
+            sleep(1) # TODO: find better way to wait for reconnect.
+            continue
+        end
         pending = Base.n_avail(nc.outbox)
         buf = IOBuffer()
         batch = pending
