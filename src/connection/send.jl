d3ec72123569444ed375fb803583ba21a03df5e7
Jakub Wronowski <jakubwro@gmail.com>
2023-10-02 19:41:25 +0200

    debug if messages are lost on client side

--- src/connection/send.jl
+++ src/connection/send.jl
@@ -1,14 +1,20 @@
 function send(nc::Connection, message::ProtocolMessage)
-    st = status(nc::Connection)
     while !isopen(nc.outbox) # TODO: this check is not threadsafe, use try catch.
         sleep(1)
     end
+    # When connection is lost outbox might be closed. Retry until reconnected.
+    #retry(put!)(nc.outbox, message)
     put!(nc.outbox, message)
 end
+
+const buffer = ProtocolMessage[]
 
 function sendloop(nc::Connection, io::IO)
     mime = MIME_PROTOCOL() 
     while true
+        if !isempty(buffer)
+            @warn "Buffer has $(length(buffer)) messages."
+        end
         if !isopen(nc.outbox)
             error("Outbox is closed.")
         end
@@ -16,8 +22,11 @@ function sendloop(nc::Connection, io::IO)
         buf = IOBuffer() # TODO: maybe this buffering is not necessary.
         batch = min(max(1, pending), 5000)
 
+        # send_buf = []
         for _ in 1:batch
             msg = take!(nc.outbox)
+            push!(buffer, msg)
+            # push!(send_buf, msg)
             if msg isa Unsub && !isnothing(msg.max_msgs) && msg.max_msgs > 0 # TODO: make more generic handler per msg type
                 @lock state.lock begin nc.unsubs[msg.sid] = msg.max_msgs end # TODO: move it somewhere else
             end
@@ -25,5 +34,6 @@ function sendloop(nc::Connection, io::IO)
         end
         write(io, take!(buf))
         flush(io)
+        empty!(buffer)
     end
 end
