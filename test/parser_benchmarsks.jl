ad925f49083b59ce322ccf94558dfc1c24b57b58
Jakub Wronowski <jakubwro@gmail.com>
2023-11-25 20:29:22 +0100

    wip on parser

--- test/parser_benchmarsks.jl
+++ test/parser_benchmarsks.jl
@@ -17,7 +17,16 @@ function bench_parser()
     io = IOBuffer(repeat(msg, 1000000))
     s = 0
     @time NATS.parser_loop(io) do res
-        @info "res" first(res)
+        @info "res" first(res) length(res)
+    end
+end
+
+function bench_parser()
+    msg = "HMSG FOO.BAR 9 34 45\r\nNATS/1.0\r\nFoodGroup: vegetable\r\n\r\nHello World\r\n"
+    io = IOBuffer(repeat(msg, 1000000))
+    s = 0
+    @time NATS.parser_loop(io) do res
+        @info "res" first(res) length(res)
     end
 end
 
@@ -42,4 +51,43 @@ function bench()
             s+= 100
         end
     end    
+end
+
+using StringViews
+function scan()
+    msg = "MSG foo 84s6LXpSRziIFL4w5Spm 16\r\n1111111111111111\r\n"
+    io = IOBuffer(repeat(msg, 1000000))
+    s = 0
+    m = false
+    cr = false
+    crlf = false
+    in_payload = false
+    pos = 0
+    ends = sizehint!(Int64[], 2000000)
+    subs = sizehint!(AbstractString[], 2000000)
+    @time buffer = readavailable(io)
+    msgs = []
+    last_end = 0
+    @time for b in buffer
+        pos += 1
+        crlf = false
+        if b == convert(UInt8, '\r')
+            cr = true
+        elseif cr && b == convert(UInt8, '\n')
+            crlf = true
+            cr = false
+        elseif b == convert(UInt8, 'M')
+            m = true
+        end
+
+        if !m && crlf
+            push!(msgs, NATS.Msg("foo", String(StringView(@view buffer[last_end+1:pos])), "", 16, "1111111111111111"))
+            # push!(msgs, SubString(StringView(@view buffer[last_end+1:pos]), 9, 29))
+            # push!(msgs, SubString(StringView(@view buffer[last_end+1:pos]), 9, 29))
+            last_end = pos
+        elseif crlf
+            m = false
+        end
+    end
+    msgs
 end
