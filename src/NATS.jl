3b145e7a8c14e9a09c61221e8291c61ce6d7b6b4
Jakub Wronowski <jakubwro@gmail.com>
2023-12-30 22:12:50 +0100

    method hints fixes

--- src/NATS.jl
+++ src/NATS.jl
@@ -62,21 +62,20 @@ function __init__()
     start_interrupt_handler()
 
     Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
-        if exc.f == convert && length(argtypes) > 1
-             # TODO: check if 2nd arg is Msg of Hmsg
+        if exc.f == convert && length(argtypes) > 1 && argtypes[2] == Type{Msg}
              print(io, """
                        
                        To use `$(argtypes[1])` as parameter of subscription handler apropriate conversion from `$(argtypes[2])` must be provided.
                        ```
                        import Base: convert
 
-                       function convert(::Type{$(argtypes[1])}, msg::NATS.Msg)
+                       function convert(::$(argtypes[1]), msg::NATS.Msg)
                            # Implement conversion logic here.
                        end
 
                        ```
                        """)
-        elseif exc.f == show
+        elseif exc.f == show && length(argtypes) > 1 && argtypes[2] == MIME_PAYLOAD
             print(io, """
                        
                        Object of type `$(argtypes[3])` cannot be serialized into payload.
