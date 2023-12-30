b6fc782b97a3fd1364717474e97b7eeb71610c35
Jakub Wronowski <jakubwro@gmail.com>
2023-12-30 22:20:04 +0100

    refactor hints

--- src/NATS.jl
+++ src/NATS.jl
@@ -54,42 +54,13 @@ include("protocol/protocol.jl")
 include("connection/connection.jl")
 include("pubsub/pubsub.jl")
 include("reqreply/reqreply.jl")
-include("interrupts.jl")
 include("experimental/experimental.jl")
+include("interrupts.jl")
+include("hints.jl")
 
 function __init__()
-
     start_interrupt_handler()
-
-    Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
-        if exc.f == convert && length(argtypes) > 1 && argtypes[2] == Type{Msg}
-             print(io, """
-                       
-                       To use `$(argtypes[1])` as parameter of subscription handler apropriate conversion from `$(argtypes[2])` must be provided.
-                       ```
-                       import Base: convert
-
-                       function convert(::$(argtypes[1]), msg::NATS.Msg)
-                           # Implement conversion logic here.
-                       end
-
-                       ```
-                       """)
-        elseif exc.f == show && length(argtypes) > 1 && argtypes[2] == MIME_PAYLOAD
-            print(io, """
-                       
-                       Object of type `$(argtypes[3])` cannot be serialized into payload.
-                       ```
-                       import Base: show
-
-                       function Base.show(io::IO, NATS.MIME_PROTOCOL, x::$(argtypes[3]))
-                            # Write content to `io` here.
-                        end
-
-                       ```
-                       """)
-        end
-    end
+    register_hints()
 end
 
 end
