47ba6a2b0c3ca3246d1c9d8a6ff3b98955fb3fe3
Jakub Wronowski <jakubwro@gmail.com>
2024-01-21 23:02:47 +0100

    fix docs

--- src/jetstream/consumer/ack.jl
+++ src/jetstream/consumer/ack.jl
@@ -1,15 +1,15 @@
-"""
-Confirms message delivery to server.
-"""
+# """
+# Confirms message delivery to server.
+# """
 function message_ack(msg::NATS.Msg; connection::NATS.Connection)
     isnothing(msg.reply_to) && error("No reply subject for msg $msg.")
     !startswith(msg.reply_to, "\$JS.ACK") && @warn "`ack` sent for message that don't need acknowledgement." 
     NATS.publish(connection, msg.reply_to)
 end
 
-"""
-Mark message as undelivered, what avoid waiting for timeout before redelivery.
-"""
+# """
+# Mark message as undelivered, what avoid waiting for timeout before redelivery.
+# """
 function message_nak(msg::NATS.Msg; connection::NATS.Connection)
     isnothing(msg.reply_to) && error("No reply subject for msg $msg.")
     !startswith(msg.reply_to, "\$JS.ACK") && @warn "`nak` sent for message that don't need acknowledgement." 
