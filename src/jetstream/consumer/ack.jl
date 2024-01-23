26ebb3527ce8780b42fd8f652ae34c20eb50d0f9
Jakub Wronowski <jakubwro@gmail.com>
2024-01-23 22:41:39 +0100

    wip on consumer

--- src/jetstream/consumer/ack.jl
+++ src/jetstream/consumer/ack.jl
@@ -1,17 +1,17 @@
 # """
 # Confirms message delivery to server.
 # """
-function message_ack(msg::NATS.Msg; connection::NATS.Connection)
+function consumer_ack(msg::NATS.Msg; connection::NATS.Connection)
     isnothing(msg.reply_to) && error("No reply subject for msg $msg.")
-    !startswith(msg.reply_to, "\$JS.ACK") && @warn "`ack` sent for message that don't need acknowledgement." 
-    NATS.request(connection, msg.reply_to)
+    !startswith(msg.reply_to, "\$JS.ACK") && @warn "`ack` sent for message that doesn't need acknowledgement." 
+    NATS.request(connection, msg.reply_to) # TODO: add retry in case of 503
 end
 
 # """
 # Mark message as undelivered, what avoid waiting for timeout before redelivery.
 # """
-function message_nak(msg::NATS.Msg; connection::NATS.Connection)
+function consumer_nak(msg::NATS.Msg; connection::NATS.Connection)
     isnothing(msg.reply_to) && error("No reply subject for msg $msg.")
-    !startswith(msg.reply_to, "\$JS.ACK") && @warn "`nak` sent for message that don't need acknowledgement." 
-    NATS.publish(connection, msg.reply_to, "-NAK")
+    !startswith(msg.reply_to, "\$JS.ACK") && @warn "`nak` sent for message that doesn't need acknowledgement." 
+    NATS.publish(connection, msg.reply_to, "-NAK")# TODO: add retry in case of 503
 end
