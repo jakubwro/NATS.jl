44995f2b06facd8450854cb52d93a369ad079051
Jakub Wronowski <jakubwro@gmail.com>
2024-01-27 12:39:23 +0100

    remove dead code

--- src/jetstream/channel/create.jl
+++
@@ -1,7 +0,0 @@
-
-function channel_stream_create(; max_msgs = 1024)
-    stream_config = StreamConfiguration(
-
-    )
-    create_stream()
-end
--- src/jetstream/channel/zero_size_channel.jl
+++
@@ -1,5 +0,0 @@
-
-# Special case for zero size
-
-
-
